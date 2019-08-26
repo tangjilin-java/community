de8eda5ff821aedb5ada62cac5f95b0550f80398
tangjilin-java <1037766906@qq.com>
2019-08-26 13:29:34 +0800

    add paging technique

--- src/main/java/ab/tjl/tscommunity/controller/IndexController.java
+++ src/main/java/ab/tjl/tscommunity/controller/IndexController.java
@@ -1,5 +1,6 @@
 package ab.tjl.tscommunity.controller;
 
+import ab.tjl.tscommunity.dto.PaginationDTO;
 import ab.tjl.tscommunity.dto.QuestionDTO;
 import ab.tjl.tscommunity.mapper.QuestionMapper;
 import ab.tjl.tscommunity.mapper.UserMapper;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -31,7 +33,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "2") Integer size) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0 ){
             for (Cookie coolie:cookies) {
@@ -46,8 +50,8 @@ public class IndexController {
             }
         }
 
-        List<QuestionDTO> questionDTOList = questionService.list();
-        model.addAttribute("question",questionDTOList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- src/main/java/ab/tjl/tscommunity/service/QuestionService.java
+++ src/main/java/ab/tjl/tscommunity/service/QuestionService.java
@@ -1,5 +1,6 @@
 package ab.tjl.tscommunity.service;
 
+import ab.tjl.tscommunity.dto.PaginationDTO;
 import ab.tjl.tscommunity.dto.QuestionDTO;
 import ab.tjl.tscommunity.mapper.QuestionMapper;
 import ab.tjl.tscommunity.mapper.UserMapper;
@@ -25,8 +26,21 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+
+        //前端页面容错处理
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+        //size*(page-1)
+        Integer offset = size * (page-1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
         for (Question question:questions) {
           User user = userMapper.findById(question.getCreator());
@@ -35,6 +49,8 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
     }
 }
