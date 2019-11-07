08ea6c1b763e9d6faad2b2e382fdfa3fe894cd26
tangjilin-java <1037766906@qq.com>
2019-11-07 13:59:00 +0800

    finished

--- src/main/java/ab/tjl/tscommunity/TscommunityApplication.java
+++ src/main/java/ab/tjl/tscommunity/TscommunityApplication.java
@@ -3,15 +3,21 @@ package ab.tjl.tscommunity;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(basePackages = "ab.tjl.tscommunity.mapper")
 @EnableScheduling
-public class TscommunityApplication {
+public class TscommunityApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TscommunityApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TscommunityApplication.class);
+    }
 }
